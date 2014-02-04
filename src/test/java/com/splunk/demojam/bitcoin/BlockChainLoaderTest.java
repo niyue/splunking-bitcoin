e3fc04e6c9e48abccefdccb9b6cc52f9ebdee86c
Ni Yue <niyue.com@gmail.com>
2014-02-05 00:07:51 +0800

    Prepare for importing bootstrap.dat file into a FullPrunedBlockStore.

---
+++ src/test/java/com/splunk/demojam/bitcoin/BlockChainLoaderTest.java
@@ -0,0 +1,32 @@
+package com.splunk.demojam.bitcoin;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.google.bitcoin.core.Block;
+import com.google.bitcoin.core.BlockChainListener;
+import com.google.bitcoin.core.FullPrunedBlockChain;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.params.MainNetParams;
+import com.google.bitcoin.store.FullPrunedBlockStore;
+import com.google.bitcoin.store.H2FullPrunedBlockStore;
+import com.google.bitcoin.utils.BlockFileLoader;
+
+public class BlockChainLoaderTest {
+	private final NetworkParameters netParams = MainNetParams.get();
+
+	@Test
+	public void testLoadBootstrapDataFile() throws Exception {
+		FullPrunedBlockStore blockStore = new H2FullPrunedBlockStore(netParams, "data/bitcoinj", 500000);
+		BlockChainListener blockChainListener = new LoggingBlockChainListener(blockStore);
+		FullPrunedBlockChain blockChain = new FullPrunedBlockChain(netParams, Arrays.asList(blockChainListener), blockStore);
+
+		BlockFileLoader loader = new BlockFileLoader(netParams, Arrays.asList(new File("/Users/sni/Library/Application Support/Bitcoin/bootstrap.dat")));
+
+		for (Block block : loader) {
+			blockChain.add(block);
+		}
+	}
+}
